b572497ae0c5b70e6a248f8d593394f899e2e0c4
predatell <seryi_belyi@inbox.ru>
2017-01-04 22:39:39 +0000

    url name favorite-add-or-remove

--- favorite/urls.py
+++ favorite/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from favorite.views import add_or_remove
 
 urlpatterns = [
-    url(r'^add-or-remove$', add_or_remove),
+    url(r'^add-or-remove$', add_or_remove, name="favorite-add-or-remove"),
 ]
